2715d4f2109a79e515194b0ddb718efe3070b607
zhan-yl <zhan_yl@163.com>
2018-12-20 15:51:26 +0800

    first commit

---
+++ cellular.go
@@ -0,0 +1,65 @@
+package cellular
+
+import (
+	"fmt"
+	"math"
+)
+
+// 是否是邻居
+func isNeighbor(p int, location int, size int) bool {
+	prow := p / size
+	pcol := p % size
+	lrow := location / size
+	lcol := location % size
+
+	if (prow-1 == lrow) && (pcol+1 == lcol || pcol == lcol || pcol-1 == lcol) {
+		return true
+	} else if (prow == lrow) && (pcol+1 == lcol || pcol-1 == lcol) {
+		return true
+	} else if (prow+1 == lrow) && (pcol+1 == lcol || pcol == lcol || pcol-1 == lcol) {
+		return true
+	}
+
+	return false
+}
+
+// 根据本身的状态及邻居的状态确定下一轮的状态
+func Islive(p int, a []int) int {
+	size := int(math.Sqrt(float64(len(a))))
+
+	var sum int
+	var state int
+	for i, s := range a {
+		if i == p {
+			state = s
+		} else if isNeighbor(p, i, size) {
+			sum += s
+		}
+	}
+	//	fmt.Println(state, sum)
+	if ((state == 1) && (sum == 2 || sum == 3)) || (state == 0 && sum == 3) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+// 细胞迭代状态
+func LiveCell(in []int) []int {
+	out := make([]int, len(in))
+	for {
+		changed := 0
+		for i, _ := range in {
+			out[i] = Islive(i, in)
+			if out[i] != in[i] {
+				changed = 1
+			}
+		}
+		in = out
+		fmt.Println(in)
+		if changed == 0 {
+			break
+		}
+	}
+	return out
+}
---
+++ cellular_test.go
@@ -0,0 +1,43 @@
+package cellular
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIslive(t *testing.T) {
+	ast := assert.New(t)
+
+	ast.Equal(0, Islive(0, []int{1, 0, 0, 0}), "2*2 test [1,1]")
+	ast.Equal(0, Islive(0, []int{1, 1, 0, 0}), "2*2 test [1,1]")
+	ast.Equal(1, Islive(0, []int{1, 1, 1, 0}), "2*2 test [1,1]")
+	ast.Equal(1, Islive(0, []int{1, 1, 1, 1}), "2*2 test [1,1]")
+	ast.Equal(0, Islive(0, []int{0, 0, 0, 0}), "2*2 test [1,1]")
+	ast.Equal(0, Islive(0, []int{0, 1, 0, 0}), "2*2 test [1,1]")
+	ast.Equal(0, Islive(0, []int{0, 1, 1, 0}), "2*2 test [1,1]")
+	ast.Equal(1, Islive(0, []int{0, 1, 1, 1}), "2*2 test [1,1]")
+
+	ast.Equal(0, Islive(1, []int{0, 1, 0, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 1, 0, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(1, Islive(1, []int{1, 1, 1, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(1, Islive(1, []int{1, 1, 1, 1, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 1, 1, 1, 1, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 1, 1, 1, 1, 1, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{0, 0, 0, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 0, 0, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 0, 1, 0, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(1, Islive(1, []int{1, 0, 1, 1, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 0, 1, 1, 1, 0, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(1, []int{1, 0, 1, 1, 1, 1, 0, 0, 0}), "3*3 test [1,2]")
+	ast.Equal(0, Islive(2, []int{1, 1, 0, 1, 0, 0, 0, 0, 0}), "3*3 test [1,2]")
+}
+
+func TestLiveCell(t *testing.T) {
+	ast := assert.New(t)
+
+	ast.Equal([]int{0, 0, 0, 0}, LiveCell([]int{1, 0, 0, 0}), "2*2 cell [1,1]")
+	ast.Equal([]int{1, 1, 1, 1}, LiveCell([]int{1, 1, 1, 0}), "2*2 cell [1,1]")
+	ast.Equal([]int{1, 1, 0, 1, 1, 0, 0, 0, 0}, LiveCell([]int{1, 1, 0, 1, 0, 0, 0, 0, 0}), "3*3 cell [1,1]")
+	ast.Equal([]int{0, 0, 0, 0, 0, 0, 0, 0, 0}, LiveCell([]int{0, 0, 0, 1, 1, 1, 0, 0, 0}), "3*3 cell [1,1]")
+}
